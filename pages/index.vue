2921b7cf3bee549c7231b5ba46cf6be2aaa9041f
Mauro Oviedo <moviedo@arsenal.com>
2022-08-12 18:28:46 -0400

    chore: update index page with base-dropdown

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
-    <dropdown></dropdown>
+    <base-dropdown :items="items">
+      <template #DropdownButton="{ onClick }">
+        <button
+          type="button"
+          class="rounded border-2 bg-red-500 py-2 px-4 font-bold text-white"
+          @click="onClick"
+        >
+          dropdown button
+        </button>
+      </template>
+    </base-dropdown>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data: () => ({
+    items: [
+      { data: "item 1" },
+      { data: "item 1" },
+      { data: "item 1" },
+      { data: "item 1" },
+      { data: "item 1" },
+      { data: "item 1" },
+    ],
+  }),
+});
+</script>
